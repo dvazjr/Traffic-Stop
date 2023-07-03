811f99d958e14cbaf0ca0b70d09f5e8c23ab8421
Daniel Vazquez <104696010+dvazjr@users.noreply.github.com>
2023-07-03 12:52:45 -0700

    Completed assignment with stop light using function method of state as well as the class method

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+
+  const [lights, setLights] = useState(["red", "black", "black"]);
+
+  const nextButton = () => {
+    if (lights[0] === "red") {
+      setLights(["black", "black", "green"]);
+    } else if (lights[1] === "yellow") {
+      setLights(["red", "black", "black"]);
+    } else if (lights[2] === "green") {
+      setLights(["black", "yellow", "black"]);
+    }
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        {lights.map((light, index) => (
+            <div className={`circle ${light}`} key={index}></div>
+          ))}
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextButton}>Next State</button>
     </div>
   );
 };
